1f0ca0d56b2e2f36bd6873dd28cc1a67bdb761ea
Metehan BOY <m3t3-han@hotmail.com>
2019-08-01 18:26:56 +0300

    Revert "Revert "program load olduktan sonra yüklenen sayfaların Page classından BasePage üretip, çağırılan sayfalara animasyon eklendi""
    This reverts commit 3f4b4613b72b39ff72b67d76078f2f426dd0d768.

---
+++ Pos2Web/Animations/PageAnimations.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Pos2Web
+{
+    public static class PageAnimations
+    {
+
+        public static async Task SlideAndFadeInFromRight(this Page page, float seconds)
+        {
+            //Create StoryBoard
+            var sb = new Storyboard();
+
+            //Add Slide From Right animation
+            sb.AddSlideFromRight(seconds, page.WindowWidth);
+
+            sb.AddFadeIn(seconds);
+            //Start Animation
+            sb.Begin(page);
+
+            //Make page visible
+            page.Visibility = Visibility.Visible;
+
+            //animasyonun bitmesini bekle
+            await Task.Delay((int)seconds * 1000);    
+        }
+        public static async Task SlideAndFadeOutToLeft(this Page page, float seconds)
+        {
+            //Create StoryBoard
+            var sb = new Storyboard();
+
+            //Add Slide From Right animation
+            sb.AddSlideToLeft(seconds, page.WindowWidth);
+
+            sb.AddFadeOut(seconds);
+            //Start Animation
+            sb.Begin(page);
+
+            //Make page visible
+            page.Visibility = Visibility.Visible;
+
+            //animasyonun bitmesini bekle
+            await Task.Delay((int)seconds * 1000);    
+        }
+
+    }
+}
