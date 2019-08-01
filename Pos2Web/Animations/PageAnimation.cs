84b94c0894f8c8b8f9147730b2dcd3a919d12c49
Metehan BOY <m3t3-han@hotmail.com>
2019-08-01 18:26:56 +0300

    Revert "Revert "program load olduktan sonra yüklenen sayfaların Page classından BasePage üretip, çağırılan sayfalara animasyon eklendi""
    This reverts commit 3f4b4613b72b39ff72b67d76078f2f426dd0d768.

---
+++ Pos2Web/Animations/PageAnimation.cs
@@ -0,0 +1,10 @@
+﻿namespace Pos2Web
+{
+    public enum PageAnimation
+    {
+        None = 0,
+        SlideAndFadeInFromRight = 1,
+        SlideAndFadeOutToLeft = 2
+
+    }
+}
--- Pos2Web/Pages/LoginPage.xaml.cs
+++ Pos2Web/Pages/LoginPage.xaml.cs
@@ -18,12 +18,16 @@ namespace Pos2Web
     /// <summary>
     /// LoginPage.xaml etkileşim mantığı
     /// </summary>
-    public partial class LoginPage : Page
+    public partial class LoginPage : BasePage
     {
         public LoginPage()
         {
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.AnimateOut();
+        }
     }
 }
